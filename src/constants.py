2cba37c1b3f81f7e810419700944aaded8422b1d
nroldanf <nicolas.roldan@mail.escuelaing.edu.co>
2024-06-30 13:23:53 -0500

    add translation logic

--- src/constants.py
+++ src/constants.py
@@ -5,4 +5,29 @@ dna_nucleobases = ["A", "C", "G", "T"]
 rna_nucleobases = ["A", "C", "G", "U"]
 # Complements: purines pairs with pyrimidines
 dna_complements = {"A": "T", "T": "A", "C": "G", "G": "C"}
-# mRNA Complements mapping (RNA transcription)
+# reverse transcription mapping
+reverse_transcription_mapping = {"U": "A", "A": "T", "G": "C", "C": "G"}
+
+# codon tables https://github.com/biopython/biopython/blob/c2ba43db03a0e95593edfe42f1b335513b4889f3/Bio/Data/CodonTable.py#L603
+# National Center for Biotechnology Information (NCBI) genetic codes: https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi
+std_codon_table = {
+    "UUU": "F", "UUC": "F", "UUA": "L", "UUG": "L",
+    "UCU": "S", "UCC": "S", "UCA": "S", "UCG": "S",
+    "UAU": "Y", "UAC": "Y",                          
+    "UGU": "C", "UGC": "C",             "UGG": "W",
+    "CUU": "L", "CUC": "L", "CUA": "L", "CUG": "L",
+    "CCU": "P", "CCC": "P", "CCA": "P", "CCG": "P",
+    "CAU": "H", "CAC": "H", "CAA": "Q", "CAG": "Q",
+    "CGU": "R", "CGC": "R", "CGA": "R", "CGG": "R",
+    "AUU": "I", "AUC": "I", "AUA": "I", "AUG": "M",
+    "ACU": "T", "ACC": "T", "ACA": "T", "ACG": "T",
+    "AAU": "N", "AAC": "N", "AAA": "K", "AAG": "K",
+    "AGU": "S", "AGC": "S", "AGA": "R", "AGG": "R",
+    "GUU": "V", "GUC": "V", "GUA": "V", "GUG": "V",
+    "GCU": "A", "GCC": "A", "GCA": "A", "GCG": "A",
+    "GAU": "D", "GAC": "D", "GAA": "E", "GAG": "E",
+    "GGU": "G", "GGC": "G", "GGA": "G", "GGG": "G",
+}
+stop_codons = [
+    "UAA", "UAG", "UGA",
+]
--- src/dna_tools.py
+++ src/dna_tools.py
@@ -23,9 +23,50 @@ def dna_mrna_transcription(dna_sequence: str) -> str:
     # Transcribe DNA to mRNA
     # Note: the dna_sequence is consider to be the coding strand and not
     # the template strand.
+    # same happens in biopython: https://biopython.org/wiki/Seq
+    # In bioinformatics we normally assume the DNA is the coding strand (not the template strand) 
+    # so this is a simple matter of replacing all the thymines with uracil:
     dna_sequence = dna_sequence.upper()
     return dna_sequence.replace("T", "U")
 
+def dna_reverse_complement(dna_sequence: str) -> str:
+    # Given the DNA strang 3'-5' (template)
+    # return the reverse complement (the 5'-3' strand)
+    # Reverse the sequence
+    dna_sequence = dna_sequence[::-1]
+    # Take the complement
+    default_symbol = "*"
+    return ''.join(dna_complements.get(symbol, default_symbol) for symbol in dna_sequence)
+
+def reverse_transcription(mrna_sequence: str) -> str:
+    # Given a mRNA sequence, use it as template to
+    # generate a strand of DNA (cDNA)
+    return "".join(reverse_transcription_mapping.get(symbol, "*") for symbol in mrna_sequence)
+
+def translation(mrna_sequence: str, stop_symbol:str="*", to_stop: bool=False) -> str:
+    # Translate mRNA into protein sequence
+    # Heavily based on biopython's behavior
+    current_index = 0
+    protein = []
+    # iterate the sequence in triplets
+    while current_index < len(mrna_sequence) - 2:
+        # Get the triplets
+        codon = mrna_sequence[current_index:current_index+3]
+        # Check if codong is a stop codon
+        if codon in stop_codons:
+            if to_stop:
+                return "".join(protein)
+            else:
+                protein.append(stop_symbol)
+                current_index += 3
+                continue
+        protein.append(std_codon_table.get(codon))
+        current_index += 3
+    return "".join(protein)
+
+
+
 if __name__ == "__main__":
-    sequence = "TTTTTGCAAAGTTAGCTGTCATAGGGCTTATGACCTCTACGACCGGGGACCAACTCTGCTTGAGCCCAATGCATAGGTTTTCCAGAATTGCATGTCCTCTATAAGTGGTTGTATTGTGGCGAAATCGCAGTTGTTTTATACGAGGATCTTCGTCTTTAATAGCTTCGGGATGTTTGTGGTGCCCATCGGAGTAGAACAGCGTTACCCGTGGATGACTGCCGTACGCCCTTGCCGACTGGGGTTCCCACTCTGTGTTCGTTCGTTCACCACAATCCGACTGGTATCGCTGGTCCGTGCAGTTATCAACGAAATAGCTGGTGTTGGCTGAGCAGACGATTTAACCAGTCGGGGTGGGCGCTAGTTCTCACCGGTATCTGGGAAGCCACTTGACGACGCGGTTGTTCCATGTAGACTCGTGGTTCTTATCCTGATGAGACGATCCGCTCTTTTTGGTGCCACCAAGACATACAACGCGGGGGTTATTACGGACCCCTTGCGAGGACAATAAGAGCCTGAGAGCGAAAAAAAGGACTGCCAGTTCTTTCTAACCCAATTAATTTTCTATTGGCTACAGTATAACGCGCCCAGAAGGGGCTGCTTCGGGGCCTGTGCTGATTTTCTTTCGTTACATTGATGGCCACTGATCGTATGCATTAATATTGAGGTCGGCCCCACCCCGCCCCCCACCGTGAAGCGCGTCCGCAAAGCCCTACACCTTCCGAAAGCTGTGCTCGCAGCCTGCCTATGGTTGTGCGGATGCACCAAAAAAGAACCAAAGACATCTTGGCAGTCCACGCCCCCGTGGAAGTTAACAAGAACGGGCTATATTCACCGTTTATGTTGCATGTCTTCGAGACACGGTAGATCCCTGAACCCATATAGTCGCAAAGTGGCGTGACGCGCCTAGGCTGACCGCAGAGTCCGTTCACTCTGACGCAGACCCGAGTGCAGGTTGTAATTCCAGGTGGAAA"
-    print(dna_mrna_transcription(sequence))
+    mrna = ""
+    print(translation(mrna, to_stop=True))
+    
