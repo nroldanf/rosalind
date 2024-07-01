ff521459c16519b1e70b9ab12eac1d735cd4e2a1
nroldanf <nicolas.roldan@mail.escuelaing.edu.co>
2024-06-30 23:00:00 -0500

    more exercises

--- src/dna_tools.py
+++ src/dna_tools.py
@@ -64,9 +64,73 @@ def translation(mrna_sequence: str, stop_symbol:str="*", to_stop: bool=False) ->
         current_index += 3
     return "".join(protein)
 
+def get_number_rna_strings(protein_sequence: str) -> int:
+    # Count the number of RNA strings that can be translated into the given protein sequence
+    # This is a simple calculation based on the standard codon table
+    # Note: This is not a real-world solution for large protein sequences
+    # because the number of RNA strings grows exponentially with the length of the protein sequence.
+    # However, for small protein sequences this should be sufficient.
+    codons_of_interest_per_aa = 0
+    for aa_in_protein in list(protein_sequence):
+        # Filter the codons that codify for the given aa
+        codons_of_interest = [codon for codon, aa_in_table in std_codon_table.items() if aa_in_protein == aa_in_table]
+        # Multiple them for the previous count to obtain all the possible number of combinations
+        # given the sequence
+        if codons_of_interest_per_aa == 0:
+            codons_of_interest_per_aa = len(codons_of_interest)
+        else:
+            codons_of_interest_per_aa *= len(codons_of_interest)
+    # Finally, multiply by the number of possible stop codons that exists
+    # to get the final number of possible combinations
+    total_rna_strings = codons_of_interest_per_aa * len(stop_codons)
+    # # Take the modulo 1M
+    # total_rna_strings %= 10**6
+    return total_rna_strings    
 
+# https://biopython.org/docs/1.75/api/Bio.SeqUtils.html#Bio.SeqUtils.GC
+def get_gc_content(dna_sequence: str) -> float:
+    # Calculate GC content in the DNA sequence
+    # i.e. porcentage of Guanine and Cytosine nucleobases
+    dna_sequence = dna_sequence.upper()
+    gc_count = sum(sequence_counter(dna_sequence, ["C", "G"]).values())
+    total_count = len(dna_sequence)
+    return (gc_count / total_count) * 100
+
+# TODO: Switch to biopython
+def read_fasta(filename: str) -> dict:
+    # Read a FASTA file and return the sequences as a dictionary
+    sequences = {}
+    with open(filename, "r") as file:
+        sequence_name = ""
+        sequence_content = ""
+        for line in file:
+            line = line.strip()
+            if line.startswith(">"):
+                if sequence_name:
+                    # Assign the complete sequence to the sequence name
+                    sequences[sequence_name] = sequence_content
+                    # Clean up the sequence
+                    sequence_content = ""
+                # Assign the name at first iteration
+                sequence_name = line[1:]
+            else:
+                # Accumulate the other part of the seuqences
+                # note: usually fasta lines are no longer than 80 symbols
+                sequence_content += line
+    sequences[sequence_name] = sequence_content
+    return sequences
+
+# Mutations: types - deletion, duplication, inversion, insertion, translocation
+# point mutations: change a base pair (changing one change the complement)
+
+def hamming_distance(sequence_1: str, sequence_2: str) -> int:
+    # Number of symbols that differ between two sequences
+    # Note: Only considers sequences of equal length
+    if len(sequence_1) == 0 or len(sequence_2) == 0:
+        return  
+    return len([symbol_1 for symbol_1, symbol_2 in zip(sequence_1, sequence_2) if symbol_1 != symbol_2])
 
 if __name__ == "__main__":
-    mrna = ""
-    print(translation(mrna, to_stop=True))
-    
+    dna1 = "AGGACTATTAAGGAGCACCCGGGTGCAGTGGGCGTTTGTATCTTCCATCGCTAGGCTGCACGCCTGTTGTTTGCCGCTGGACGTTATACTAAGCTCACCGATTTATCCAGAAGCTTGCACGGGCCCATTGATCACCTTGGGCTACGAGTGGCCTCGACAAATAGTGATGCCCGACAAGGCACATATCCGGTGCCAGCGGAACTACCTCTACCGTGCAAGTAACCACAACGCAAGTTAGCGCAGGTATCTATGCTCGTCATAAGGGCAGGGTAACGGTAGCCGAGGCGGCAACTCTTTTGTAAGTGCGTGGAACGGCGATTAGAGCAGGCCCGCGTAACAAGCACTAGTCCGTGACGACACATAATGGCTGATAAGACTGCCTCGTGAAAGGACACAGGGGCCTTGAGTCCTCCGGGTGGTATCTATTCGTTTGCTATTAGGAGTCGGGCCCTGTCTCTCACTCGTACCGCTGCCTCCAGTCTTAGATTTCCCAGACGGAACGGGATAATGTCTCACGGGGAGATATAGTGTAAACTTAGGGGTCCCAGCACTTTCCTGCCTCACAAAATCGCTGTCGGCTGTCTACGTGTGTTTCTCCGGGAATACCTCGCTCACCGTATGAGAAATGTTGCTTTCAGTTTTGAGGTAGTGATTATGATGGGAAAGACAACTTTTAGTATAGTCTAGGCACTGTGATATCGAGAGGTGCAACCGAGGCGTCTAGGTTAGGAATCAGGGCCGCACGCCGGGTAACACTAGACCAGGAACGGTCACCTAGTCCCTCTTGCGAGTCGCACGTCGAAGCTTCAACTGTGCGAGAAGTTCATACATTAAAGAGATCCGTTAGAAGGTGCAACGTGCTGTCCGCTTAATATATCCGCAGGCGTCTGCGGTATGGCGAGCCGCTTCGGCTTTATTTATCA"
+    dna2 = "CGAATTCTTTTCGAGTTTCTTGAAACAGGAAAATTTTAAGTCCATCGTCCCTCGCGTCCGTTTTGCTGGTTTGCCTCACTTCGTCAGACAAGCCTCGGCGATTTTGTCTCCAGCTGACCGAAGCGCAAGGATAACCGGAGGCAGGGCGGGACCACGGGCATCAGTCAAACTAGACAAGCTCCTTATCGCGAAAGAACTGGAAATACTCTTCCGTGCACTCAAGCAAATAGCAAATTAGCGCAGCTAACCAGTCTCTAGAAGTTGCACGTCTAACGCAAACCGTCACGAGCGCGCTTATACAGGTCTGCAATACGGCAGTGAGGGCGGCTGTGAACAGTTATCACTTGCTGCCGCGGAAGCACAACTGATAGTTAAACAACTACGTTGTAGCGGATCTGGGCCTTTATTACTTTCGGTGATTCCTCCTACTATTCCAACATGAGGCGGGGACTGGCGAATTATAGCAGGGCAGTTGCAGGACCTAGCTCTTCCATATTGGTATCTATGTGGACGCAAGGGGTAACAACGTCTAAACCTGTTTATGCTTTCGTTCGCCTGCAAAAGAAGATCTCCGCGTACCATCGCTCGCTTTTGGTCTGTGCTAACCGCCGTAACCGTATTAGAAATGTACCATCCAGAAATGCGCGCCATAGTCTGAAGTTACAGCTCACTGTTCGGATATGTTAGAGACTGTTCCGTCCACGCGTGCCCCAGATGCGCCGAGCCTCGTTATAATGGCAACACGTTGTGTATCCGACTAACGGGCATCGGTACACATTGACTGGCGAGACTCGCTCTTCGTTGCTTGAGCTCCGTACGTACGACAAGGATTAAAGATACTCAAGCCTAGCGGTAAAGGTTCTCCCTCTTAGTTGCGCCGCCGGAGACGGAGGATTGGTTAGCCGCTCTACTAGGTGTTGACA"
+    print(hamming_distance(dna1, dna2))
---
+++ src/dynamic_programming.py
@@ -0,0 +1,10 @@
+if __name__ == "__main__":
+    n_months = 5
+    k_rabbit_pairs = 3
+    
+    rabbits_at_ith_month = 0
+    for i in range(1, n_months+1):
+        rabbits_at_nth_month = rabbits_at_nth_month + i
+        
+        
+    
